c80380ec36c9b2b3a59c6c8a4b1d4ea783ffbf41
itachsheep <itachsheep@163.com>
2017-12-30 13:10:49 +0800

    创建第一个ndk demo

---
+++ FirstNdkDemo/src/com/tao/firstndk/Cryptor.java
@@ -0,0 +1,11 @@
+package com.tao.firstndk;
+
+public class Cryptor {
+	public native static void crypt(String normal_path, String crypt_path);
+	public native static void decrypt(String crypt_path, String decrypt_path);
+	
+	static{
+		System.loadLibrary("ndk_file_crypt");
+	}
+	
+}
